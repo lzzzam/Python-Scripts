1c3fc8a08f230a5c815c14f280059d52fa75fd69
lzzzam <58335995+lzzzam@users.noreply.github.com>
2022-01-21 00:50:20 +0100

    Semi working version with port selection and Autoscroll

--- Projects/comm.py
+++ Projects/comm.py
@@ -1,49 +1,63 @@
-import time
+import ctypes
 import threading
 from tkinter import *
 import serial
 
-ser = serial.Serial('/dev/cu.usbmodem1421')
+lock_thread = threading.Lock()
+lock_serial = threading.Lock()
+lock_window = threading.Lock()
 
+ser = serial.Serial(timeout=1)
 stop_serial_thread = False
+stop_main_thread = False
 
-class serial(threading.Thread):
-    def __init__(self, name, input, monitor, autoscroll):
+
+class serialPlotter(threading.Thread):
+    def __init__(self, name, monitor, autoscroll):
       threading.Thread.__init__(self)
       self.name = name
-      self.input = input
       self.monitor = monitor
       self.autoscroll = autoscroll
-      self.TimeCounter = time.time()
-      self.LineCounter = 0
-            
+      self.char = ""
+      self.plot = False
+      
     def receive(self):
-        chr = ser.read()
-        self.monitor.config(state=NORMAL)
-        self.monitor.insert(END,chr)
-        if self.autoscroll.get() == True:
-            self.monitor.see("end")
-        self.monitor.config(state=DISABLED)
-        
-    # def receive(self): #Mocked
-        # if((time.time() - self.TimeCounter) > 0.1):
-        #     newLine = f"new line #{self.LineCounter}\n"
-        #     self.monitor.config(state=NORMAL)
-        #     self.monitor.insert(END,newLine)
-        #     if self.autoscroll.get() == True:
-        #         self.monitor.see("end")
-        #     self.monitor.config(state=DISABLED)
-        #     self.TimeCounter = time.time()
-        #     self.LineCounter += 1
+        lock_serial.acquire()
+        if(ser.is_open == True and ser.in_waiting > 0):
+            self.chr = ser.read()
+            self.plot = True
+        lock_serial.release()
         
     def run(self):
+        lock_thread.acquire()
         print ("Starting " + self.name)
-        while(stop_serial_thread == False):
+        
+        while(True):
             self.receive()
+            if (stop_serial_thread == True):
+                break
+            else:
+                lock_window.acquire()
+                if (self.plot == True):
+                    self.monitor.config(state=NORMAL)
+                    self.monitor.insert(END,self.chr)
+                    if self.autoscroll.get() == True:
+                        self.monitor.see("end")
+                    self.monitor.config(state=DISABLED)
+                    self.plot = False
+                lock_window.release()
+                
+        if (ser.is_open == True):
+            ser.flush
+            ser.flushInput()
+            ser.close()
+            
         print("Ending " + self.name)
+        lock_thread.release()
         
-        
-def transmitt(entry):
-    str = entry.get()
-    if str != "":
-        ser.write(str.encode('ascii'))
+            
+def transmitt(entry=None):
+    if(ser.is_open == True):
+        str = entry.get()
+        if str != "":
+            ser.write(str.encode('ascii'))
